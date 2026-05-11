fb70b0d69710cc80bcceb1be61e3a8c8f1bcf6d4
Azaria-git <azariaravelomanana@gmail.com>
2026-05-11 17:25:58 +0300

    Refactor MariaDB Docker setup: update base image, remove unused files, and add init-db script

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Initialize MariaDB data directory
-mysql_install_db --user=mysql --datadir=/var/lib/mysql
-
-# Start MariaDB
-mysqld_safe
---
+++ srcs/requirements/mariadb/tools/init-db.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+service mariadb start
+
+mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
+
+mysql -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+
+mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
+
+mysql -e "FLUSH PRIVILEGES;"
+
+mysqladmin -u root shutdown
+
+exec mysqld --console
